9186af520575daa704f36f7f33a747c6e280358b
PiDexter <christiaan_wiggers@yahoo.com>
2021-05-14 19:13:09 +0200

    Create Application, routes and run

--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
+use app\src\Application;
+
 require_once __DIR__ . '/../autoload.php';
 
+$app = new Application(dirname(__DIR__));
 
+$app->router->get('/', function () {
+    return "hello";
+});
+
+$app->router->get('/contact', 'index');
+
+$app->run();
