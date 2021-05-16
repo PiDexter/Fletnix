87b2066e7dcd4340056b1948d6405a8811df6eba
PiDexter <christiaan_wiggers@yahoo.com>
2021-05-16 22:14:45 +0200

    Add template tag parser

--- app/src/Router.php
+++ app/src/Router.php
@@ -75,40 +75,44 @@ class Router
     }
 
 
-    public function renderView($view, $data = [])
+    public function renderView($view, $data = []): array|bool|string
     {
         // Haal de main layout op
         $layout = $this->layoutContent();
-//        $view = $this->renderOnlyView($view, $data);
+        $view = $this->renderOnlyView($view, $data);
 
-        $view = $this->parseViewTags($view, $data);
-
-        // Tag in html is gelijk aan de parameter naam
-        // Parameter 'name' met waarde 'peter'
-        // Vervangt in html {{name}} voor de waarde
-//        foreach ($data as $key => $value) {
-//            $view = str_replace("{{" . $key . "}}", (string) $value, $view);
-//        }
+        // Vervang alle {{var}} & {{key->value}} tags in de view
+        $view = $this->replaceViewTags($view, $data);
 
         // Vervang de string "{{content}}" met de inhoud van de view file in de layout
         return str_replace("{{content}}", $view, $layout);
     }
 
-    public function parseViewTags($view, $data = [])
+    // Proces een tag bijvoorbeeld: {{name}}
+    private function replaceViewTags($view, $data)
     {
-
-        $view = $this->renderOnlyView($view, $data);
-
         foreach ($data as $key => $value) {
-            $view = str_replace("{{" . $key . "}}", (string) $value, $view);
+            if (is_array($value)) {
+                $view = $this->nestedViewTag($view, $key, $value);
+            } else {
+                $view = str_replace("{{" . $key . "}}", (string) $value, $view);
+            }
         }
+        return $view;
+    }
 
+    // Proces een nested tag bijvoorbeeld: {{user->name}}
+    private function nestedViewTag($view, $parentName, $data)
+    {
+        foreach ($data as $key => $value) {
+            $view = str_replace("{{" . $parentName . "->" . $key. "}}", (string) $value, $view);
+        }
         return $view;
     }
 
 
-
-    protected function layoutContent() {
+    protected function layoutContent(): bool|string
+    {
 
         // Bewaard alles in een string - ook wel: output buffer - voorkomt directe weergave
         ob_start();
@@ -120,17 +124,17 @@ class Router
         return ob_get_clean();
     }
 
-    public function renderOnlyView($view, $data) {
+    public function renderOnlyView($view, $data): bool|string
+    {
 
         // Zet parameter key als variable naam met als waarde de value
         extract($data);
 
-
         // Bewaar alles in een string - ook wel: output buffer - voorkomt directe weergave
         ob_start();
-
         //  Haal view op
         include_once Application::$ROOT_DIR."/views/$view.php";
+
         // Returns de waarde (content in dit geval) die in de buffer zit en leegt vervolgens de output buffer
         return ob_get_clean();
     }
