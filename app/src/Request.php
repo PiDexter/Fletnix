667081ad554bd4bc40b522f02b1d84ff1616969f
PiDexter <christiaan_wiggers@yahoo.com>
2021-05-17 14:49:27 +0200

    Change methods used in getBody

--- app/src/Request.php
+++ app/src/Request.php
@@ -45,16 +45,18 @@ class Request
 
     public function getBody(): array
     {
+        // Lege body array
         $body = [];
 
-        if ($this->method() === 'get') {
+        if ($this->isGet()) {
             foreach ($_GET as $key => $value) {
                 $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
             }
         }
 
-        if ($this->method() === 'post') {
+        if ($this->isPost()) {
             foreach ($_POST as $key => $value) {
+                var_dump($key);
                 $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
             }
         }
