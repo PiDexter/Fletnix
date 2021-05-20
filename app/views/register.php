f5c1774e19e040c8f4d786f96f302b07b99da9a0
PiDexter <christiaan_wiggers@yahoo.com>
2021-05-20 15:04:01 +0200

    Change textfield name to email

--- app/views/register.php
+++ app/views/register.php
@@ -40,8 +40,8 @@
             <div class="row">
                 <div class="column">
                     <div class="input-block fullwidth">
-                        <input type="email" class="input-textField" name="textField" placeholder="E-mail" required>
-                        <label for="textField" class="input-label">E-mail</label>
+                        <input type="email" class="input-textField" name="email" placeholder="E-mail" required>
+                        <label for="email" class="input-label">E-mail</label>
                     </div>
                 </div>
             </div>
