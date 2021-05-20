73947be86009be25193448d1d6453a726de8cad7
PiDexter <christiaan_wiggers@yahoo.com>
2021-05-20 21:00:04 +0200

    Add login method

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -4,18 +4,32 @@
 namespace app\controllers;
 
 
+use app\models\User;
 use app\src\Application;
 use app\src\Controller;
+use app\src\Request;
 
 class AuthController extends Controller
 {
 
     public function index() {
-
         $params = [
             'name' => "Chris"
         ];
         return $this->render('login', $params);
+    }
+
+    public function login(Request $request)
+    {
+        $request = $request->getBody();
+        $user = new User();
+
+        if(!empty($request) && $user->exists($request['email'])) {
+            if(password_verify($request['password'], $user->fetch()['password'])) {
+                $_SESSION['user'] = $user->fetch();
+                Application::$app->response->redirect('/');
+            }
+        }
     }
 
 }
