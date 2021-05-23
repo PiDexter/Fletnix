746fe6d6e5a91da09051c9b8785224751ecee2c9
PiDexter <christiaan_wiggers@yahoo.com>
2021-05-23 11:53:00 +0200

    Refactored response method

--- app/src/Router.php
+++ app/src/Router.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace app\src;
 
 use app\controllers\MovieController;
+use ReflectionClass;
 use ReflectionMethod;
 
 class Router
@@ -39,73 +40,61 @@ class Router
         $this->routes['post'][$path] = $callback;
     }
 
-    public function createDynamicRoute(array $params)
-    {
-        if (count($params) > 1) {
-            $this->get('/movie/' . $params[1], [MovieController::class, 'show']);
+
+    public function routeMatch($path, $method, $urlFragments) {
+
+//            // Alle keys uit $routes[] als $route
+//            // Voorbeeld:
+//            // [
+//            // '/',
+//            // '/movie',
+//            // '/movie/:id'
+//            // ]
+        foreach (array_keys($this->routes[$method]) as $route) {
+            $routes = explode('/', $route);
+            array_shift($routes);
+            var_dump($routes);
+
+            // Wanneer in de routes array een route matched met de eerst url param
+            // EN deze route ook matched met een ":" dan staat er een variabele in de route
+            // Zet nu het huidige pad (bijv: movies/5) naar "movies/:id"
+            // Omdat movies/5 geen route in de routes array is maar movies/id wel
+            if (strpos($route, $urlFragments[0]) && strpos($route, ':')) {
+                $path = $route;
+            }
+
         }
+        return $this->routes[$method][$path] ?? false;
     }
 
 
 
-
+    /**
+     * @throws \ReflectionException
+     */
     public function resolve()
     {
         // Ontvang het pad (string) van de request (voorbeeld pad: /contact)
         $path = $this->request->getPath();
         // Welke methode is gebruikt? (get, post etc)
         $method = $this->request->method();
-        // Welke parameters heeft de url
-        $urlParams = $this->request->getParams($path);
 
-        var_dump($path);
-        var_dump(count($urlParams));
-        var_dump($urlParams[0]);
+        // Verkrijg de url fragments in array
+        $urlFragments = $this->request->getUrlFragments($path);
 
-        echo '-----';
-
-        var_dump($path);
-        foreach (array_keys($this->routes[$method]) as $route) {
-            $routes = explode('/', $route);
-            array_shift($routes);
-
-            if (strpos($route, 'movie') && strpos($route, ':')) {
-//                $test[] = $route;
-//                $route = explode('/', filter_var(rtrim($route, '/')), FILTER_SANITIZE_URL);
-//                $route[2] = 5;
-//                $match[] = str_replace(':id', '4', array_keys($this->routes[$method]));
-//                $this->routes[$method][$route] = $route;
-                var_dump($route);
-                $path = $route;
-            }
-        }
-
-        var_dump($path);
-//        var_dump($this->routes);
-
-
-
-
-
-
-
-//        // Als het url pad groter is dan 1, dan zijn de volgende stukken parameters
-//        if (count($urlParams) > 1) {
-//            $this->get('/movie/' . $urlParams[1], [MovieController::class, 'show']);
-//        }
-
-
-
+        // Check of de route bestaat
         $callback = $this->routes[$method][$path] ?? false;
 
-        var_dump($callback);
-
-        // Als route niet bestaat
+        // Als route niet bestaat bekijk of er een route is die dynamisch is
         if (!$callback) {
 
-            // Zet de status code van de response op 404 (not found)
-            $this->response->setStatusCode(404);
-            return "Not found";
+            $callback = $this->routeMatch($path, $method, $urlFragments);
+
+            // Als er geen dynamische route bestaat
+            if (!$callback) {
+                $this->response->setStatusCode(404);
+                return "Not found";
+            }
         }
 
         // Als de callback geen functie maar string is, return dan een view
@@ -114,31 +103,34 @@ class Router
         }
 
         if (is_array($callback)) {
-            // Instantieer een specifieke controller instantie
+            // Instantieert een specifieke controller instantie
             Application::$app->controller = new $callback[0]();
             $callback[0] = Application::$app->controller;
         }
 
 
-        $r = new ReflectionMethod($callback[0], $callback[1]);
-        $params = $r->getParameters();
-        $parameters = [];
-        foreach ($params as $param) {
-            $parameters[] = $param->getName();
-            var_dump($param);
+        $reflection = new ReflectionMethod($callback[0], $callback[1]);
+        // Kijk welke parameters de controller methode vraagt
+        $params = $reflection->getParameters();
+
+        if (!empty($params)) {
+            $parameters = [];
+            foreach ($params as $param) {
+                // Als een parameters gelijk is aan 'request', geef het request object dan terug
+                // TODO dit is een tijdelijke workaround voor het matchen en resolve van method parameters
+                if (strpos((string)$param, 'request')) {
+                    $parameters[] = $this->request;
+                } else if (strpos((string)$param, 'id')) {
+                    $parameters[] = (int) $urlFragments[1];
+                } else {
+                    continue;
+                }
+
+            }
+            return call_user_func_array($callback, $parameters);
         }
 
-//        $test[] = $this->request;
-        $test[] = $urlParams[1];
-
-//        var_dump($parameters);
-
-
-//        return call_user_func_array($callback, array());
-        return call_user_func_array($callback, $test);
-
-        // Voer methode die bij het pad in de array routes hoort uit
-//        return call_user_func($callback, $this->request);
+        return $callback($this->request, $this->response);
     }
 
     public function view($view, $data = [])
