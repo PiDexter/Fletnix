5de33b5f9a453eb2e5104cd420e24059f9977473
PiDexter <christiaan_wiggers@yahoo.com>
2021-05-25 20:18:47 +0200

    Add getMovies() by movie_genre

--- app/models/Genre.php
+++ app/models/Genre.php
@@ -5,8 +5,18 @@ namespace app\models;
 
 
 use app\src\Model;
+use PDO;
 
 class Genre extends Model
 {
+    protected string $primaryKey = 'name';
 
+    public function getMovies()
+    {
+        return $this->builder->select(['*'], $this->getTable())
+            ->join('movie_genre', ['genre.genre_name' => 'movie_genre.genre_name'])
+            ->query()->fetchAll(PDO::FETCH_ASSOC);
+
+//        return $this->builder->select(['*'], $this->getTable())->query()->fetchAll();
+    }
 }
