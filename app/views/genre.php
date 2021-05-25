99cbf0ee876c38c4659d40f98f661bed27baafd0
PiDexter <christiaan_wiggers@yahoo.com>
2021-05-25 20:19:08 +0200

    Add some basic card sample code to view

--- app/views/genre.php
+++ app/views/genre.php
@@ -1,4 +1,15 @@
 <?php
 ?>
 
-{{genreName}}
+<section id="genres">
+    <div class="container">
+        <div class="card-grid">
+            <?php foreach($data as $genre) : ?>
+                <a href="/genre/<?php echo $genre['genre_name']; ?>" class="card">
+                    <?php echo $genre['genre_name']; ?>
+                </a>
+            <?php endforeach; ?>
+        </div>
+    </div>
+
+</section>
