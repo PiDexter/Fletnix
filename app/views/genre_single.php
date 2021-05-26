9598952d14e97ca45f3a1a78c59d98f9e67f49a4
Christiaan Wiggers <christiaan_wiggers@yahoo.com>
2021-05-26 09:16:48 +0200

    Get all movies by genre in view

--- app/views/genre_single.php
+++ app/views/genre_single.php
@@ -1,1 +1,15 @@
 <?php
+?>
+
+<section id="genres">
+    <div class="container">
+        <div class="card-grid">
+            <?php foreach($data as $movie) : ?>
+                <a href="/genre/<?php echo $movie['title']; ?>" class="card">
+                    <?php echo $movie['title']; ?>
+                </a>
+            <?php endforeach; ?>
+        </div>
+    </div>
+
+</section>
