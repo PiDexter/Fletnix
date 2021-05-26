7b3e8beba575472c92cebefc5c69605b4eb2b0e7
Christiaan Wiggers <christiaan_wiggers@yahoo.com>
2021-05-26 12:01:01 +0200

    Change href to movie id

--- app/views/genre_single.php
+++ app/views/genre_single.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="card-grid">
             <?php foreach($data as $movie) : ?>
-                <a href="/genre/<?php echo $movie['title']; ?>" class="card">
+                <a href="/movie/<?php echo $movie['movie_id']; ?>" class="card">
                     <?php echo $movie['title']; ?>
                 </a>
             <?php endforeach; ?>
