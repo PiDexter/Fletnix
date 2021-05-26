4938fb15c1dcce3c79c7f3a92f1e74ea9703c0c5
Christiaan Wiggers <christiaan_wiggers@yahoo.com>
2021-05-26 09:04:01 +0200

    Use allWith() to retrieve allMovies

--- app/models/Genre.php
+++ app/models/Genre.php
@@ -5,18 +5,13 @@ namespace app\models;
 
 
 use app\src\Model;
-use PDO;
 
 class Genre extends Model
 {
     protected string $primaryKey = 'name';
 
-    public function getMovies()
+    public function getMovies(): array
     {
-        return $this->builder->select(['*'], $this->getTable())
-            ->join('movie_genre', ['genre.genre_name' => 'movie_genre.genre_name'])
-            ->query()->fetchAll(PDO::FETCH_ASSOC);
-
-//        return $this->builder->select(['*'], $this->getTable())->query()->fetchAll();
+        return $this->allWith('movie_genre', ['genre.genre_name' => 'movie_genre.genre_name']);
     }
 }
