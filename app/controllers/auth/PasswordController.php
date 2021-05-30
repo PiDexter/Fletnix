140d89ee901d7bff2173b7ad9d9900c9188b33de
PiDexter <christiaan_wiggers@yahoo.com>
2021-05-30 12:08:55 +0200

    Verify and handle change password

--- app/controllers/auth/PasswordController.php
+++ app/controllers/auth/PasswordController.php
@@ -6,7 +6,9 @@ use app\middleware\AuthMiddleware;
 use app\models\User;
 use app\src\Application;
 use app\src\Controller;
+use app\src\Model;
 use app\src\Request;
+use app\src\session\FlashMessage;
 
 class PasswordController extends Controller
 {
@@ -15,7 +17,7 @@ class PasswordController extends Controller
         $this->setMiddleware(new AuthMiddleware());
     }
 
-    public function index()
+    public function index(): bool|array|string
     {
         return $this->render('auth/edit_password');
     }
@@ -24,14 +26,28 @@ class PasswordController extends Controller
     {
         $formData = $request->getBody();
 
-        // TODO: VALIDATE CURRENT PASSWORD AND PROCESS UPDATE
-
-        $data = [
-            'password' => password_hash($formData['newPassword'], PASSWORD_DEFAULT),
-        ];
+        // Haal de huidige gebruiker op uit de database
         $user = (new User)->findByID(Application::$app->session->get('user'));
-        (new User)->update($user['user_id'], $data);
 
-        Application::$app->response->redirect('/profile');
+        // Als het huidige wachtwoord matched, dan kan het gewijzigd worden met het nieuwe wachtwoord
+        if (password_verify($formData['currentPassword'], $user['password'])) {
+
+            // Check of het nieuwe wachtwoord gelijk is aan het conformatie wachtwoord
+            if ($formData['newPassword'] === $formData['confirmPassword']) {
+
+                $data = [
+                    'password' => password_hash($formData['newPassword'], PASSWORD_DEFAULT),
+                ];
+
+                // Bewaar nieuw wachtwoord
+                (new User)->update($user['user_id'], $data);
+            } else {
+                FlashMessage::error('Nieuw wachtwoord komt niet overeen.');
+            }
+        } else {
+            FlashMessage::error('Onjuist wachtwoord.');
+        }
+        return $this->render('auth/edit_password');
     }
+
 }
