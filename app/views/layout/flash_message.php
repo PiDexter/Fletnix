82ef477c19331e8243cb0e043d8dd4b8d92fc483
PiDexter <christiaan_wiggers@yahoo.com>
2021-05-30 22:15:48 +0200

    Use hasFlashMessage() method

--- app/views/layout/flash_message.php
+++ app/views/layout/flash_message.php
@@ -1,5 +1,11 @@
+<?php
+use app\src\session\FlashMessage;
+?>
+
+<?php if (FlashMessage::hasFlashMessage()) : ?>
 <div class="row">
     <div class="column alert {{messageType}}">
         <span>{{errorMessage}}</span>
     </div>
 </div>
+<?php endif; ?>
