35718f967719c862f1b96d94b94b87b4ba167929
PiDexter <christiaan_wiggers@yahoo.com>
2021-05-31 20:31:12 +0200

    update search form with new fields

--- app/views/search_filter.php
+++ app/views/search_filter.php
@@ -5,44 +5,53 @@
 <section id="searchFilter">
     <div class="container">
 
-        <form action="" method="post">
+        <form action="/results" method="post">
             <div class="row">
                 <div class="column">
                     <div class="input-block fullwidth">
                         <input type="text" name="title" class="" placeholder="Zoek in titel">
-                        <label for="title">Titel bevat</label>
+                        <label for="title" class="input-label">Titel bevat</label>
                     </div>
                 </div>
             </div>
 
-            <select name="genre" class="">
-                <option value="genre">Opties</option>
+            <select name="genre" class="" >
+                <option value="Action" selected>Opties</option>
             </select>
             <label for="genre">Genre</label>
 
             <div class="row">
                 <div class="column">
                     <div class="input-block fullwidth">
-                        <input type="number" class="input-textField" name="startDate" maxlength="4" placeholder="Vanaf jaartal">
-                        <label for="startDate" class="input-label">Vanaf jaartal</label>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="input-block fullwidth">
-                        <input type="number" class="input-textField" name="endDate" maxlength="4" placeholder="Tot jaartal">
-                        <label for="endDate" class="input-label">Tot jaartal</label>
+                        <input type="number" class="input-textField" name="publicationYear" maxlength="4" placeholder="Publicatiejaar">
+                        <label for="publicationYear" class="input-label">Publicatiejaar</label>
                     </div>
                 </div>
             </div>
 
-            <select name="genre" class="">
-                <option value="genre">Opties</option>
-            </select>
-            <label for="genre">Genre</label>
+            <div class="row">
+                <div class="column">
+                    <div class="input-block fullwidth">
+                        <input type="text" name="firstName" class="" placeholder="Voornaam">
+                        <label for="firstName" class="input-label">Voornaam</label>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="input-block fullwidth">
+                        <input type="text" name="lastName" class="" placeholder="Achternaam">
+                        <label for="lastName" class="input-label">Achternaam</label>
+                    </div>
+                </div>
+            </div>
+
+<!--            <select name="genre" class="">-->
+<!--                <option value="genre">Opties</option>-->
+<!--            </select>-->
+<!--            <label for="genre">Genre</label>-->
 
             <div class="row">
                 <div class="column">
-                    <button type="submit" class="btn btn-primary fullwidth">Search</button>
+                    <button type="submit" class="btn btn-primary fullwidth">Zoeken</button>
                 </div>
             </div>
         </form>
