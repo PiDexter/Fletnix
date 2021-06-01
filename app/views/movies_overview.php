080c6cdc2c65db7e703bfa6c68a3e8812498db52
PiDexter <christiaan_wiggers@yahoo.com>
2021-06-01 21:23:05 +0200

    Enhance view for all movies

--- app/views/movies_overview.php
+++ app/views/movies_overview.php
@@ -1,11 +1,16 @@
 <?php
 /* @var $data array */
 ?>
-<section id="searchResult">
+<section id="allMovies">
     <div class="container">
-
-        @pagination
-
+        <div class="row">
+            <div class="column">
+                <span class="page-count">Pagina <?php echo $_GET['page'] ?? 1; ?></span>
+            </div>
+            <div class="column">
+                <a href="/search" class="btn btn-secondary">Uitgebreid zoeken</a>
+            </div>
+        </div>
         <div class="card-grid">
             <?php foreach($data as $movie) : ?>
                 <a href="/movie/<?php echo $movie['movie_id']; ?>" class="card">
@@ -13,6 +18,7 @@
                 </a>
             <?php endforeach; ?>
         </div>
+        @pagination
     </div>
 </section>
 
