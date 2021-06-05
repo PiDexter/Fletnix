b0f42fa8f1dfb04ee1d79ce0efc95aa747418848
PiDexter <christiaan_wiggers@yahoo.com>
2021-06-05 12:43:02 +0200

    Limit with pagination

--- app/models/Genre.php
+++ app/models/Genre.php
@@ -11,16 +11,18 @@ class Genre extends Model
 {
     protected string $primaryKey = 'name';
 
-    public function getMovies()
+    public function getMovies(): array
     {
         return $this->allWith('movie_genre', ['genre.genre_name' => 'movie_genre.genre_name']);
     }
 
-    public function getMoviesByGenre(string $genre)
+    public function getMoviesByGenre(string $genre, int $page): array
     {
         return $this->builder->select(['*'], 'movie')
             ->join('movie_genre', ['movie.movie_id' => 'movie_genre.movie_id'])
             ->where('movie_genre.genre_name', '=', $genre)
+            ->orderBy(['movie.movie_id'], 'ASC')
+            ->limit(($page-1) * $page, $this->perPage)
             ->query()->fetchAll(PDO::FETCH_ASSOC);
     }
 }
