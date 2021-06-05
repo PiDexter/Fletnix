a105f1ef45d2d00e2611cad92965f25a8b62730c
PiDexter <christiaan_wiggers@yahoo.com>
2021-06-05 12:43:10 +0200

    Add title

--- app/views/genre.php
+++ app/views/genre.php
@@ -3,6 +3,7 @@
 
 <section id="genres">
     <div class="container">
+        <h1>Genres</h1>
         <div class="card-grid">
             <?php foreach($data as $genre) : ?>
                 <a href="/genre/<?php echo $genre['genre_name']; ?>" class="card">
