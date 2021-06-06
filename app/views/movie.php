b331a59bc9fa82ad881ac061de3265a24a927d86
PiDexter <christiaan_wiggers@yahoo.com>
2021-06-06 20:31:20 +0200

    Enhance single movie page html/css

--- app/views/movie.php
+++ app/views/movie.php
@@ -6,16 +6,23 @@
     </video>
     <div class="spotlight-row">
         <h2 class="spotlight-title">{{title}}</h2>
-        <p>Time icon: {{duration}} min.</p>
-        <div class="column content-left">
-            <button class="btn btn-secondary fullwidth">Afspelen</button>
+        <div class="column content-left vertical-center">
+            <img src="/images/clock-icon.svg" width="16" height="16" alt="" class="icon">
+            <span>{{duration}} min.</span>
         </div>
+
+<!--        <div class="column content-left">-->
+<!--            <button class="btn btn-secondary fullwidth">Afspelen</button>-->
+<!--        </div>-->
     </div>
 </section>
 <section id="details">
     <div class="container">
         <div class="row">
-            <p>{{description}}</p>
+            <div class="column content-left">
+                <button class="btn btn-secondary fullwidth ">
+                    <img src="/images/play-button-black-icon.svg" width="24" height="24" alt="" class="icon">Afspelen</button>
+            </div>
         </div>
     </div>
 </section>
@@ -24,17 +31,60 @@
     <div class="tabs-wrapper">
         <input type="radio" name="tabs" id="tab-1" checked="checked">
         <label for="tab-1" class="tab-label">Details</label>
-        <div class="tab">
-            <dl id="movie-details">
-                <dt>Director</dt>
-                <dd>{{director}}</dd>
+        <div class="tab" id="movie-details">
 
-                <dt>Publicatiejaar</dt>
-                <dd>{{publication_year}}</dd>
+            <div class="row">
+                <div class="column content-left vertical-center">
+                    <img src="/images/director-icon.svg" width="24" height="24" alt="" class="icon">
+                    <span class="column-title">Regisseur</span>
+                </div>
+                <div class="column">
+                    <span>{{director}}</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="column content-left vertical-center">
+                    <img src="/images/calendar-icon.svg" width="24" height="24" alt="" class="icon">
+                    <span class="column-title">Publicatiejaar</span>
+                </div>
+                <div class="column">
+                    <span>{{publication_year}}</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="column content-left vertical-center">
+                    <img src="/images/director-icon.svg" width="24" height="24" alt="" class="icon">
+                    <span class="column-title">Samenvatting</span>
+                </div>
+                <div class="column">
+                    <span>{{description}}</span>
+                </div>
+            </div>
 
-                <dt>Samenvatting</dt>
-                <dd>{{description}}</dd>
-            </dl>
+
+
+<!--            <dl>-->
+
+<!--                <div class="row">-->
+<!--                    <div class="column content-left vertical-center">-->
+<!--                        <img src="/images/director-icon.svg" width="24" height="24" alt="" class="icon">-->
+<!--                        <dt>Director:</dt>-->
+<!--                        <dd>{{director}}</dd>-->
+<!--                    </div>-->
+<!--                </div>-->
+
+<!--                <div class="column content-left vertical-center">-->
+<!--                    <img src="/images/director-icon.svg" width="24" height="24" alt="" class="icon">-->
+<!--                    <dt>Director:</dt>-->
+<!--                    <dd>{{director}}</dd>-->
+<!--                </div>-->
+
+<!--                <dt>Publicatiejaar</dt>-->
+<!--                <dd>{{publication_year}}</dd>-->
+<!---->
+<!--                <dt>Samenvatting</dt>-->
+<!--                <dd>{{description}}</dd>-->
+<!--            </dl>-->
         </div>
 
         <input type="radio" name="tabs" id="tab-2">
