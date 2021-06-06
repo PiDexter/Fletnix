2555db81ad6516fc115d1adf3df9ca04ae2b2375
PiDexter <christiaan_wiggers@yahoo.com>
2021-06-06 16:50:20 +0200

    Separate query

--- app/models/Movie.php
+++ app/models/Movie.php
@@ -6,31 +6,54 @@ namespace app\models;
 
 use app\src\Model;
 use app\src\QueryBuilder;
+use PDO;
 
 class Movie extends Model
 {
 
-    public function getAll($page)
+    public function getMovieDetails($id): array
     {
         $builder = new QueryBuilder();
         $builder->select(['*'], $this->getTable());
         $builder->join('movie_genre', ['movie.movie_id' => 'movie_genre.movie_id']);
         $builder->join('movie_director', ['movie.movie_id' => 'movie_director.movie_id']);
         $builder->join('person', ['movie_director.person_id' => 'person.person_id']);
-        $builder->orderBy(['movie.movie_id'], 'ASC');
-        $builder->limit($page, $this->perPage);
-
-        return $builder->query()->fetchAll();
+        $builder->where("movie." . $this->getPrimaryKey(), '=', $id);
+        return $builder->query()->fetch();
     }
 
-    public function getMovies()
+    public function getMovieDirectorByID($id)
+    {
+        $this->builder->select(['*'], 'person');
+        $this->builder->join('movie_director', ['person.person_id' => 'movie_director.person_id']);
+        $this->builder->where("movie_director.movie_id", '=', $id);
+        $director = $this->builder->query()->fetch();
+
+        if (!$director) {
+            $director = 'Onbekend';
+        } else {
+            $director = $director['firstname'] . " " . $director['lastname'];
+        }
+
+        return $director;
+    }
+
+
+    public function getCastByID($id): array
     {
         $builder = new QueryBuilder();
-        $builder->select(['*'], $this->getTable());
-        $builder->join('movie_genre', ['movie.movie_id' => 'movie_genre.movie_id']);
-        $builder->join('movie_director', ['movie.movie_id' => 'movie_director.movie_id']);
-        $builder->join('person', ['movie_director.person_id' => 'person.person_id']);
-        return $builder->query()->fetchAll();
+        $builder->select(['*'], 'movie_cast');
+        $builder->join('person', ['movie_cast.person_id' => 'person.person_id']);
+        $builder->where("movie_cast.movie_id", '=', $id);
+        $castList = $builder->query()->fetchAll(PDO::FETCH_ASSOC);
+
+        // When no cast is found, return empty array
+        if (!$castList) {
+            $castList = array();
+        }
+
+        return $castList;
     }
+
 
 }
