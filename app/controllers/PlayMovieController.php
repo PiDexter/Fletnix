cc4f2a96558e4745310422b5af3f29749dc5bd45
PiDexter <christiaan_wiggers@yahoo.com>
2021-06-06 16:50:52 +0200

    Update show method

--- app/controllers/PlayMovieController.php
+++ app/controllers/PlayMovieController.php
@@ -15,20 +15,24 @@ class PlayMovieController extends Controller
         $this->setMiddleware(new AuthMiddleware());
     }
 
-    public function show(int $id): bool|array|string
+    public function show(int $id)
     {
-        $movie = (new Movie())->findByID($id);
+        $movie = new Movie();
+
+        $movieDetails = $movie->findByID($id);
 
         // Als movie niet gevonden kan worden
-        if (!$movie) {
+        if (!$movieDetails) {
             return $this->render('404');
         }
 
         $viewData = [
-            'title' => $movie['title'],
-            'duration' => $movie['duration'],
-            'description' => $movie['description'],
-            'publicationYear' => $movie['publication_year']
+            'title' => $movieDetails['title'],
+            'duration' => $movieDetails['duration'],
+            'description' => $movieDetails['description'],
+            'publication_year' => $movieDetails['publication_year'],
+            'director' => $movie->getMovieDirectorByID($id),
+            'movie_cast' => $movie->getCastByID($id)
         ];
 
         return $this->render('movie', $viewData);
