922d84b8591f22d2ba106cc34e5728f9d4d0f2d0
PiDexter <christiaan_wiggers@yahoo.com>
2021-06-10 14:32:31 +0200

    Add validation (WIP)

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -9,17 +9,68 @@ use app\src\Application;
 use app\src\Controller;
 use app\src\Model;
 use app\src\Request;
+use app\src\Validator;
 
 class RegisterController extends Controller
 {
-    public function index()
+    public function index(): bool|array|string
     {
         return $this->render('register');
     }
 
-    public function create(Request $request)
+    public function create(Request $request): bool|array|string
     {
         $request = $request->getBody();
+        $errors = [];
+
+        $rules = [
+            'first_name' => ['required', ],
+            'email' => ['required', 'type:email']
+        ];
+
+
+        $validator = new Validator();
+        $validate = $validator->validate($request, $rules);
+//
+//        var_dump($request);
+
+//        foreach ($request as $key => $value) {
+//
+//            if (array_key_exists($key, $rules)) {
+//
+//                foreach ($rules[$key] as $rule) {
+//
+//                    switch ($rule) {
+//                        case 'required':
+//                            if (empty($value)) {
+//                                $errors[] = 'Required field cannot be empty';
+//                            }
+//                            break;
+//
+//                        case 'type:email':
+//                            if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+//                                $errors[] = 'Email address is not valid';
+//                            }
+//                            break;
+//
+//                        case 'numeric':
+//                            if (!is_numeric($value)) {
+//                                $errors[] = 'Only numbers are accepted';
+//                            }
+//                            break;
+//
+//                        case 'string':
+//                            if (!is_string($value)) {
+//                                $errors[] = 'Only letters are accepted';
+//                            }
+//                            break;
+//                    }
+//                }
+//
+//            }
+//        }
+
+;
 
         // TODO: return error messages
 
@@ -32,6 +83,13 @@ class RegisterController extends Controller
             'date_of_birth' => $request['date_of_birth'],
             'bank_number' => $request['bank_number'],
         ];
+
+
+        if ($validator->hasErrors()) {
+            $user[] = $validate;
+            var_dump($user);
+            return $this->render('register', $user);
+        }
 
         if ($request['password'] !== $request['confirm_password']) {
             return $this->render('register', $user);
--- app/src/Validator.php
+++ app/src/Validator.php
@@ -7,6 +7,64 @@ namespace app\src;
 class Validator
 {
 
-    public $rules;
+    public array $errors = [];
+
+    /**
+     * @return array
+     */
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+    public function hasErrors(): bool
+    {
+        return !empty($this->errors);
+    }
+
+    public function validate(array $request, array $rules): bool|array
+    {
+        foreach ($request as $key => $value) {
+
+            if (array_key_exists($key, $rules)) {
+
+                foreach ($rules[$key] as $rule) {
+
+                    switch ($rule) {
+                        case 'required':
+                            if (empty($value)) {
+                                $this->errors[$key] = 'Required field cannot be empty';
+                            }
+                            break;
+
+                        case 'type:email':
+                            if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                                $this->errors[$key] = 'Email address is not valid';
+                            }
+                            break;
+
+                        case 'numeric':
+                            if (!is_numeric($value)) {
+                                $this->errors[$key] = 'Only numbers are accepted';
+                            }
+                            break;
+
+                        case 'string':
+                            if (!is_string($value)) {
+                                $this->errors[$key] = 'Only letters are accepted';
+                            }
+                            break;
+                    }
+                }
+
+            }
+        }
+
+        if (empty($this->errors)) {
+            return true;
+        }
+
+        return $this->errors;
+    }
 
 }
