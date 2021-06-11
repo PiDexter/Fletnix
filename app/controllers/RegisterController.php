af62f668f35786ce418f2b62419b53cd41b401e5
PiDexter <christiaan_wiggers@yahoo.com>
2021-06-11 13:04:00 +0200

    Validate input data

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -22,15 +22,13 @@ class RegisterController extends Controller
         $request = $request->getBody();
 
         $rules = [
-            'first_name' => ['required', ['min' => 1], ['max' => 30], 'string'],
-            'last_name' => ['required', ['min' => 1], ['max' => 30], 'string'],
-            'email' => ['required', 'type:email', 'unique:email'],
+            'first_name' => ['required', ['min' => 1], ['max' => 40], 'string'],
+            'last_name' => ['required', ['min' => 1], ['max' => 40], 'string'],
+            'email' => ['required', 'email', 'unique:email'],
+            'country' => ['required', 'string'],
             'password' => ['password'],
+            'bank_number' => ['required', 'numeric', ['min' => 8], ['max' => 20]]
         ];
-
-
-        $validator = new Validator();
-        $errors = $validator->validate($request, $rules);
 
         $user = [
             'email' => $request['email'],
@@ -42,15 +40,14 @@ class RegisterController extends Controller
             'bank_number' => $request['bank_number'],
         ];
 
+        $validatedData = new Validator($request, $rules);
 
-        if ($validator->hasErrors()) {
-            $user['error'] = $errors;
-            return $this->render('register', $user);
+        if ($validatedData->hasErrors()) {
+            $user['error'] = $validatedData->getErrors();
+        } else {
+            (new User)->create($user);
+            Application::$app->response->redirect('/login');
         }
-
-
-        (new User)->create($user);
-        Application::$app->response->redirect('/login');
 
         return $this->render('register', $user);
     }
