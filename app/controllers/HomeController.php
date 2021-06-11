824ccedaa157a3d850b0235298e897f07b8f7795
PiDexter <christiaan_wiggers@yahoo.com>
2021-06-11 13:12:11 +0200

    Refactor to use Movie methods

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,25 +2,15 @@
 
 namespace app\controllers;
 
+use app\models\Movie;
 use app\src\Controller;
-use app\src\QueryBuilder;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $trending = (new QueryBuilder())
-            ->select(['movie.movie_id', 'movie.title', 'movie_genre.genre_name'], 'movie')
-            ->join('movie_genre', ['movie.movie_id' => 'movie_genre.movie_id'])
-            ->limit(10, 6)
-            ->query()->fetchAll();
-
-        $documentary = (new QueryBuilder())
-            ->select(['movie.movie_id', 'movie.title', 'movie_genre.genre_name'], 'movie')
-            ->join('movie_genre', ['movie.movie_id' => 'movie_genre.movie_id'])
-            ->where('movie_genre.genre_name', '=', 'Documentary')
-            ->limit(0, 6)
-            ->query()->fetchAll();
+        $trending = (new Movie)->trending();
+        $documentary = (new Movie)->getByGenre('Documentary');
 
         $data = [
             'trending' => $trending,
