423dddcb23ebc23d0039f6ff699c71e8d9cc8aa9
PiDexter <christiaan_wiggers@yahoo.com>
2021-06-14 13:27:27 +0200

    Change methods to private and add htmlspecialchars

--- app/src/core/View.php
+++ app/src/core/View.php
@@ -17,7 +17,6 @@ class View
         // Vervang alle {{var}} & {{key->value}} tags in de view
         $view = $this->replaceViewTags($view, $data);
         $view = $this->renderPagination($view, $data);
-
         $view = $this->removeEmptyTags($view);
 
         // Vervang de string "{{content}}" met de inhoud van de view file in de layout
@@ -31,7 +30,7 @@ class View
             if (is_array($value)) {
                 $view = $this->nestedViewTag($view, $key, $value);
             } else {
-                $view = str_replace("{{" . $key . "}}", $value, $view);
+                $view = str_replace("{{" . $key . "}}", htmlspecialchars($value), $view);
             }
         }
         return $view;
@@ -57,13 +56,13 @@ class View
         return $view;
     }
 
-    protected function renderPagination($view, $data): array|string
+    private function renderPagination($view, $data): array|string
     {
         $paginationView = $this->renderOnlyView('layout/components/pagination', $data);
         return str_replace("@pagination", (string) $paginationView, $view);
     }
 
-    protected function layoutContent(): bool|string
+    private function layoutContent(): bool|string
     {
         // Bewaard alles in een string - ook wel: output buffer - voorkomt directe weergave
         ob_start();
@@ -75,7 +74,7 @@ class View
         return ob_get_clean();
     }
 
-    public function renderOnlyView($view, $data): bool|string
+    private function renderOnlyView($view, $data): bool|string
     {
         // Zet parameter key als variable naam met als waarde de value
         extract($data, EXTR_OVERWRITE);
