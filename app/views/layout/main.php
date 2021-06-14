e858631d09c48112f71a8be7208b778ede53de7a
PiDexter <christiaan_wiggers@yahoo.com>
2021-06-14 15:24:42 +0200

    Add favicon

--- app/views/layout/main.php
+++ app/views/layout/main.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Title</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <link rel="stylesheet" type="text/css" href="/css/stylesheet.css">
+    <link rel="shortcut icon" type="image/png" href="/images/favicon-32x32.png"/>
+    <title>Fletnix</title>
 </head>
 <body>
 <div id="grid-container">
