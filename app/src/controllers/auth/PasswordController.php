a055ec3d710ec01e8dfbe1c4552eb6a29d5ec081
PiDexter <christiaan_wiggers@yahoo.com>
2021-06-16 11:55:24 +0200

    Validate change password

--- app/src/controllers/auth/PasswordController.php
+++ app/src/controllers/auth/PasswordController.php
@@ -5,6 +5,7 @@ namespace app\src\controllers\auth;
 use app\src\core\Application;
 use app\src\core\Controller;
 use app\src\core\Request;
+use app\src\core\Validator;
 use app\src\middleware\AuthMiddleware;
 use app\src\models\User;
 
@@ -20,32 +21,32 @@ class PasswordController extends Controller
         return $this->render('user/edit_password');
     }
 
-    public function update(Request $request)
+    public function update(Request $request): bool|array|string
     {
         $formData = $request->getBody();
-
-        // Haal de huidige gebruiker op uit de database
         $user = (new User)->findByID(Application::$app->session->get('user'));
 
-        // Als het huidige wachtwoord matched, dan kan het gewijzigd worden met het nieuwe wachtwoord
-        if (password_verify($formData['currentPassword'], $user['password'])) {
+        $rules = [
+            'password' => ['required', 'password:confirm'],
+            'confirm_password' => ['required'],
+        ];
+        $validatedData = new Validator($formData, $rules);
 
-            // Check of het nieuwe wachtwoord gelijk is aan het conformatie wachtwoord
-            if ($formData['newPassword'] === $formData['confirmPassword']) {
 
-                $data = [
-                    'password' => password_hash($formData['newPassword'], PASSWORD_DEFAULT),
-                ];
+        if (!$validatedData->hasErrors() && password_verify($formData['current_password'], $user['password'])) {
 
-                // Bewaar nieuw wachtwoord
-                (new User)->update($user['user_id'], $data);
-            } else {
-//                FlashMessage::error('Nieuw wachtwoord komt niet overeen.');
-            }
-        } else {
-//            FlashMessage::error('Onjuist wachtwoord.');
+            $data = [
+                'password' => password_hash($formData['password'], PASSWORD_DEFAULT),
+            ];
+
+            (new User)->update($user['user_id'], $data);
+
+            $data['message'] = 'Password changed successfully';
+            return $this->render('user/edit_password', $data);
         }
-        Application::$app->response->redirect('/profile/change-password');
+
+        $data['error'] = $validatedData->getErrors();
+        return $this->render('user/edit_password', $data);
     }
 
 }
--- app/views/user/edit_password.php
+++ app/views/user/edit_password.php
@@ -5,18 +5,20 @@
 
         <form method="POST">
 
+            {{message}}
             <div class="input-block fullwidth">
-                <input type="password" name="currentPassword" id="currentpassword-input" placeholder="Current password" required>
+                <input type="password" name="current_password" id="currentpassword-input" placeholder="Current password" required>
                 <label for="currentpassword-input" class="input-label">Current password</label>
             </div>
 
+            {{error->password}}
             <div class="input-block fullwidth">
-                <input type="password" name="newPassword" id="newpassword-input" placeholder="New password" required>
+                <input type="password" name="password" id="newpassword-input" placeholder="New password" required>
                 <label for="newpassword-input" class="input-label">New password</label>
             </div>
 
             <div class="input-block fullwidth">
-                <input type="password" name="confirmPassword" id="confirmpassword-input" placeholder="Confirm password" required>
+                <input type="password" name="confirm_password" id="confirmpassword-input" placeholder="Confirm password" required>
                 <label for="confirmpassword-input" class="input-label">Confirm password</label>
             </div>
 
